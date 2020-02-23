2d82459860fe7e601b325dfb218ab0294c68d003
Hacker Y2J <60312092+HACK3RY2J@users.noreply.github.com>
2020-02-23 16:39:25 +0530

    Update PBomb.sh

--- PBomb.sh
+++ PBomb.sh
@@ -2,8 +2,12 @@
 clear
 echo -e "\e[4;31m Panda Hackers !!! \e[0m"
 echo -e "\e[1;34m Presents \e[0m"
+echo -e "\e[4;31m A Special \e[0m"
+echo -e "\e[4;31m Ultra Fast!!! \e[0m"
+echo -e "\e[4;31m Smooth!!! \e[0m"
+echo -e "\e[4;31m Bomber \e[0m"
 echo -e "\e[1;32m PBomb \e[0m"
-echo "Press Enter To Begin"
+echo "Press Enter To Launch"
 read a1
 if [[ -s update.pandahackers ]];then
 echo "All Requirements Found...."
@@ -68,8 +72,8 @@ rm update.pandahackers >> temp
 rm temp
 chmod +x PBomb.sh
 fi
-echo -e "\e[1;32m PBomb Refreshing Now..."
-echo -e "\e[1;32m All The Required Packages Will Be Installed..."
+echo -e "\e[1;32m PBomb Will Refresh Now..."
+echo -e "\e[1;32m All The Required Packages Have Been Installed..."
 echo -e "\e[1;34m Press Enter To Refresh..."
 read a6
 ./PBomb.sh
@@ -79,19 +83,18 @@ clear
 echo -e "\e[1;33m"
 figlet PBomb
 echo -e "\e[1;34mCreated By \e[1;34m"
-toilet -f mono12 -F border Panda Hackers
+toilet -f mono12 -F border PH
 echo  " "
 echo -e "\e[1;32m                   Features\e[1;34m"
-echo "  [+] Unlimited And Ultra-Fast-Smooth Bombing"
-echo "  [+] International Bombing"
-echo "  [+] Call Bombing "
-echo "  [+] Protection List"
-echo "  [+] Automatic Future Updates"
-echo "  [+] Easy To Use And Embed in Code"
+echo "  Unlimited And Ultra-Fast-Smooth Bombing"
+echo "  International Bombing"
+echo "  Call Bombing "
+echo "  Automatic Future Updates"
+echo "  Easy To Use And Embed in Code"
 echo -e "\e[1;32m                   Contributors\e[1;33m"
-echo -e "\e[1;33m      [*]  Instagram   \e[1;31m"
+echo -e "\e[1;33m      [*]  Panda Hackers   \e[1;31m"
 echo "         [-] Mail At: pandahacker127@gmail.com"
-echo -e "\e[1;33m      [*]  HACK3RY2J   \e[1;31m"
+echo -e "\e[1;33m      [*]  Instagram   \e[1;31m"
 echo "         [-] Ping At: http://Instagram.com/panda_Hackers_Official/"
 echo -e "\e[1;33m      [*]  Telegram   \e[1;31m"
 echo "         [-] Ping At: https://t.me/PandaHackers/"
@@ -101,15 +104,16 @@ echo ""
 echo ""
 echo -e "\e[1;31m This is Only For Educational Purposes or To Prank.\e[0m"
 echo -e "\e[1;31m Do not Use This To Irritate Others. \e[0m"
-echo -e "\e[1;31m I Am Not Responsible For The Misuse Of The Script. \e[0m"
-echo -e "\e[1;32m Update it If It Doesn't Work.\e[0m"
+echo -e "\e[1;31m Do not Use This To Harm Others. \e[0m"
+echo -e "\e[1;31m We Are Not Responsible For The Misuse Of The Script. \e[0m"
+echo -e "\e[1;32m Update If Doesn't Work.\e[0m"
 echo  " "
 echo -e "\e[4;31m That's All !!!\e[0m"
-echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
-echo -e "\e[1;32m           Mail: pandahacker127@gmail.com \e[0m"
+echo -e "\e[1;34m For Any Queries Mail Us!!!\e[0m"
+echo -e "\e[1;32m            Mail: pandahacker127@gmail.com \e[0m"
 echo -e "\e[1;32m       Instagram: https://Instagram.com/Panda_Hackers_official/ \e[0m"
-echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/PandaHackers \e[0m"
-echo "Press Enter To Go Home"
+echo -e "\e[4;32m    YouTube Page: https://www.youtube.com/c/PandaHackers \e[0m"
+echo "Press Enter To Return To Main Menu"
 read a3
 clear
 elif [ $ch -eq 5 ];then
@@ -117,17 +121,23 @@ clear
 echo -e "\e[1;31m"
 figlet PBomb
 echo -e "\e[1;34m Created By \e[1;32m"
-toilet -f mono12 -F border Panda Hackers
-echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
-echo -e "\e[1;32m           Mail: pandahackers127@gmail.com \e[0m"
+toilet -f mono12 -F border PH Squad
+echo -e "\e[1;34m For Any Queries Mail Us!!!\e[0m"
+echo -e "\e[1;32m            Mail: pandahackers127@gmail.com \e[0m"
 echo -e "\e[1;32m       Instagram: https://instagram.com/Panda_Hackers_official/ \e[0m"
-echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/PandaHackers/ \e[0m"
+echo -e "\e[4;32m    YouTube Page: https://www.youtube.com/c/PandaHackers/ \e[0m"
 echo " "
 exit 0
 else
-echo -e "\e[4;32m Invalid Input !!! \e[0m"
-echo "Press Enter To Go Home"
+echo -e "\e[4;32m Invalid Input Detected !!! \e[0m"
+echo "Press Enter To Return To Main Menu"
 read a3
 clear
 fi
 done
+
+
+
+
+
+
